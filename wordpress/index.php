aa3d8fb0b2b13f47be0a7f9f6ab7b3a0a0570f08
Diovane <Diovane@PC-Dhyow.ifprpvai.lan>
2016-06-01 16:46:13 -0300

    adição do link do banco de dados

--- wordpress/index.php
+++ wordpress/index.php
@@ -15,3 +15,4 @@ define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php'             );
+// link do banco de dados: https://drive.google.com/folderview?id=0BwCJGeQhsqUQR1ZIV19fTDJ1MTQ&usp=sharing
