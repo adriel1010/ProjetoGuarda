f51efaddfbed8f758f104800f6934e001e69f349
Aluno <Aluno@LAB5-MC07>
2016-11-10 12:04:16 -0200

    Atualzação wordpress e alguns plugins

--- wordpress/index.php
+++ wordpress/index.php
@@ -14,8 +14,4 @@
 define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
-
-require( dirname( __FILE__ ) . '/wp-blog-header.php   ');
-
-// link do banco de dados: https://drive.google.com/folderview?id=0BwCJGeQhsqUQR1ZIV19fTDJ1MTQ&usp=sharing
-
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
